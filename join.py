ecd0b70a98f3a9a9ff8d6f2c4f5a4e81f2a85f56
Richard <richard.balakrishnan@syncfusion.com>
2022-10-03 14:12:21 +0530

    included inner_join.csv

---
+++ join.py
@@ -0,0 +1,9 @@
+import pandas as pd
+salesforce = pd.read_csv('sample1.csv', encoding="windows-1251")
+boldreports = pd.read_csv('sample2.csv', encoding="windows-1251")
+# here we using inner join for getting common data from salesforce and boldreports csv
+inner_join = pd.merge(salesforce, boldreports, how='inner')
+# del inner_join[inner_join.columns.values[0]]
+inner_join.to_csv('inner_join.csv')
+diff = salesforce.compare(boldreports)
+print(diff)
