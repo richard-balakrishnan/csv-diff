5f652bb1f25460b7edc8128206745da91a957757
Richard <richard.balakrishnan@syncfusion.com>
2022-10-04 22:37:21 +0530

    dummy data

--- join.py
+++ join.py
@@ -1,9 +1,20 @@
 import pandas as pd
-salesforce = pd.read_csv('report-boldreports.csv', encoding="windows-1251")
-boldreports = pd.read_csv('report-salesforce.csv', encoding="windows-1251")
+salesforce = pd.read_csv('sample1.csv', encoding="ascii")
+boldreports = pd.read_csv('sample1.csv', encoding="ascii")
+# since we dont have id for unique, create an column with row number
+salesforce.insert(0, 'id', range(0, 0 + len(salesforce)))
+boldreports.insert(0, 'id', range(0, 0 + len(boldreports)))
+
+print(salesforce)
+print(boldreports)
+
 # here we using inner join for getting common data from salesforce and boldreports csv
-inner_join = pd.merge(salesforce, boldreports, how='inner')
+inner_join = pd.merge(salesforce, boldreports, on='id', how='inner')
+
+print(inner_join)
+# inner_join = inner_join.iloc[: , 1:]
 # del inner_join[inner_join.columns.values[0]]
-inner_join.to_csv('inner_join.csv')
-diff = salesforce.compare(boldreports)
-print(diff)
+inner_join.to_csv('join.csv')
+# diff = salesforce.compare(inner_join)
+# print(salesforce)
+# print(inner_join)
